1fee6e815dcc7343e4b60a718e413eeb0a9d4455
Alexander Bokov <alexander.bokov30@yandex.ru>
2024-03-27 16:07:37 +0300

    Исправление типа возвращаемого значения функции BmiService

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public double calculate(double weightKg, double heightMeters) {
+    public int calculate(double weightKg, double heightMeters) {
         double result = weightKg / (heightMeters * heightMeters);
         return (int) result;
 
